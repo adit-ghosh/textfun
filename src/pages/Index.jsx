765af9d7e8721cc6e25bf054a28dfae3ff95f7f6
Adit Ghosh <blubrdpirts@gmail.com>
2025-06-06 20:55:42 +0530

    Major Changes toward the website

---
+++ src/pages/Index.jsx
@@ -0,0 +1,8 @@
+export default function Index() {
+  return (
+    <div>
+      <h1>Welcome to the Index Page</h1>
+      <p>This is the main entry point of your application.</p>
+    </div>
+  );
+}
